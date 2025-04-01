6f574548a2ec20dbb864386ee108c45b407cf047
Gokul Nath A <gokulnath0827@gmail.com>
2025-04-01 18:03:17 +0530

    drizzle orm and neon db setup, users schema

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,15 @@
-import 'dotenv/config';
+import { loadEnvConfig } from '@next/env';
+
 import { defineConfig } from 'drizzle-kit';
+
+const { combinedEnv } = loadEnvConfig(process.cwd());
+console.log('hello', loadEnvConfig(process.cwd()));
 
 export default defineConfig({
   out: './src/db/migrations',
   schema: './src/db/schema.ts',
   dialect: 'postgresql',
   dbCredentials: {
-    url: process.env.NEXT_APP_DATABASE_URL!,
+    url: combinedEnv.NEXT_APP_NEON_DATABASE_URL!,
   },
 });
